343f677b4c7d4a9d402997a58a8e38a72ab4d21e
Jonathan Barbeau <jbarbeau@o2web.ca>
2018-02-23 11:23:12 -0500

    added blank

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
-set :stage, :staging
-set :branch, :staging
+set :stage, :dev
+set :branch, :develop
 
 # Simple Role Syntax
 # ==================
@@ -9,15 +9,16 @@ set :branch, :staging
 
 # Extended Server Syntax
 # ======================
-server 'example.com', user: 'deploy', roles: %w{web app db}
+server '160.153.72.129', user: 'jbarbeau89', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
-set :ssh_options, {
-  port: 522
-}
+#set :ssh_options, {
+  # keys: %w(~/.ssh/id_rsa_deployer),
+  # forward_agent: true
+  # auth_methods: %w(password)
+#}
 
-fetch(:default_env).merge!(wp_env: :staging)
-
+fetch(:default_env).merge!(wp_env: :development)
