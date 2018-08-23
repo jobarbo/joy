98e158d24911c1be92f3c2d88b24570c275ee895
Jonathan Barbeau <jobarbo@me.com>
2018-08-22 20:21:47 -0400

    ready to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,7 @@ set :branch, :master
 
 # Extended Server Syntax
 # ======================
-server 'example.com', user: 'deploy', roles: %w{web app db}
+server '160.153.72.129', user: 'jbarbeau89', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
